92e3fc86568c44d2f6a269fae0f55d43b2893c29
Stephen Vincent <stevevincent17@icloud.com>
2024-04-05 12:37:39 +0100

    Connected to Prisma and Supabase DB. Users can now login and there can be now duplicate users.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,21 +7,28 @@ import {
   LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import UserNav from "./UserNav";
 
 export async function Navbar() {
-  const { isAuthenticated } = getKindeServerSession();
+  const { isAuthenticated, getUser } = getKindeServerSession();
+  const user = await getUser();
+
   return (
     <nav className="border-b bg-background h-[10vh] flex items-center">
       <div className="container flex items-center justify-between">
         <Link href="/">
-          <h1 className="font-bold text-3xl">VincentSass</h1>
+          <h1 className="font-bold text-3xl">
+            Vincent<span className="text-primary">Saas</span>
+          </h1>
         </Link>
         <div className="flex items-center gap-x-5">
           <Themetoggle />
           {(await isAuthenticated()) ? (
-            <LogoutLink>
-              <Button>Log out</Button>
-            </LogoutLink>
+            <UserNav
+              email={user?.email as string}
+              image={user?.picture as string}
+              name={user?.given_name as string}
+            />
           ) : (
             <div className="flex items-center gap-x-5">
               <LoginLink>

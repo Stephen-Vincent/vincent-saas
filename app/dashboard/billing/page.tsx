c1d2e945e72a6445ce5ccbf9f7f6a07b2d124bec
Stephen Vincent <stevevincent17@icloud.com>
2024-04-05 16:11:36 +0100

    created payment feature, created success and cancelled routes

--- app/dashboard/billing/page.tsx
+++ app/dashboard/billing/page.tsx
@@ -1,6 +1,11 @@
+import prisma from "@/app/lib/db";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { CheckCircle2 } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { getStripeSession } from "@/app/lib/stripe";
+import { redirect } from "next/navigation";
+import { StripeSubscriptionCreationButton } from "@/app/components/SubmitButtons";
 
 const featureItems = [
   {
@@ -20,7 +25,52 @@ const featureItems = [
   },
 ];
 
-export default function BillingPage() {
+async function getData(userId: string) {
+  const data = await prisma.subscription.findUnique({
+    where: {
+      userId: userId,
+    },
+    select: {
+      status: true,
+      user: {
+        select: {
+          stripeCustomerId: true,
+        },
+      },
+    },
+  });
+
+  return data;
+}
+
+export default async function BillingPage() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  const data = await getData(user?.id as string);
+
+  async function createSubscription() {
+    "use server";
+
+    const dbUser = await prisma.user.findUnique({
+      where: {
+        id: user?.id,
+      },
+      select: {
+        stripeCustomerId: true,
+      },
+    });
+    if (!dbUser?.stripeCustomerId) {
+      throw new Error("Unable to get customer id");
+    }
+
+    const subscriptionUrl = await getStripeSession({
+      customerId: dbUser.stripeCustomerId,
+      domainUrl: "http://localhost:3000",
+      priceId: process.env.STRIPE_PRICE_ID as string,
+    });
+
+    return redirect(subscriptionUrl);
+  }
   return (
     <div className="max-w-md mx-auto space-y-4">
       <Card className="flex flex-col">
@@ -50,8 +100,8 @@ export default function BillingPage() {
             ))}
           </ul>
 
-          <form className="w-full">
-            <Button className="w-full">Buy Now</Button>
+          <form className="w-full" action={createSubscription}>
+            <StripeSubscriptionCreationButton />
           </form>
         </div>
       </Card>
